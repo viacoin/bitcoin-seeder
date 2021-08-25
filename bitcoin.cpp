9517c483e8869e9aa36e5da407a7337f2c35261c
romanornr <romanornr@gmail.com>
2021-08-25 22:45:47 +0200

    [Viacoin] adjustments viacoin core

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/bitcoin-seeder:0.01/";
+    string ver = "/viacoin-seeder:0.01/";
     uint8_t fRelayTxs = 0;
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight << fRelayTxs;
     EndMessage();
